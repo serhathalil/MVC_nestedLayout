b936b7245f9b7f72028cb0b35d31a79b3d30519f
Serhat Silsüpür <serhatsilsupur@gmail.com>
2017-11-24 14:35:20 +0300

    mvc layout 1.versiyon

---
+++ layoutKullanimi/layoutKullanimi/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace layoutKullanimi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Arsiv()
+        {
+            return View();
+        }
+        public ActionResult Arananlar()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+        public ActionResult Referanslar()
+        {
+            return View();
+        }
+
+        public ActionResult Kecioren()
+        {
+            return View();
+        }
+        public ActionResult Cankaya()
+        {
+            return View();
+        }
+        public ActionResult Yenimahalle()
+        {
+            return View();
+        }
+        public ActionResult Mamak()
+        {
+            return View();
+        }
+    }
+}
---
+++ layoutKullanimi/layoutKullanimi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="layoutKullanimi.MvcApplication" Language="C#" %>
